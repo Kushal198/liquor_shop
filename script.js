d25c4f1e3eacfebd7be64c2368e8be7f9a01810e
kushal198 <kushalkc198@gmail.com>
2023-05-02 11:10:10 +0545

    initial

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
-let menu = document.getElementById('nav');
-let open = document.getElementById('menu-btn');
-let close = document.getElementById('close');
+let menu = document.getElementById("nav");
+let open = document.getElementById("menu-btn");
+let close = document.getElementById("close");
 
-function openmenu(){
-    menu.style.left="0";
-    open.style.display="none";
-    close.style.display="block";
+function openmenu() {
+  menu.style.left = "0";
+  open.style.display = "none";
+  close.style.display = "block";
+}
+function closemenu() {
+  menu.style.left = "-100%";
+  open.style.display = "block";
+  close.style.display = "none";
+}
 
-}
-function closemenu(){
-    menu.style.left="-100%";
-    open.style.display="block";
-    close.style.display="none";
-}
+let navLink = document.getElementById("");
 
-function change(){
-    var nav=document.getElementById("navbar");
-    var value=window.scrollY;
-    if(value>85)
-    {
-        nav.classList.add("nav-change");
-    }
-    else
-    {
-        nav.classList.remove("nav-change");
-    }
+function change() {
+  var nav = document.getElementById("navbar");
+  var value = window.scrollY;
+  if (value > 85) {
+    nav.classList.add("nav-change");
+  } else {
+    nav.classList.remove("nav-change");
+  }
 }
-window.addEventListener('scroll',change);
+window.addEventListener("scroll", change);
